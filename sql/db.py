ed64e69248c899c07eab63285cbd1ac5dd0c012d
killerinvincible69 <87910385+killerinvincible69@users.noreply.github.com>
2021-08-07 11:04:03 +0530

    Update db.py

--- sql/db.py
+++ sql/db.py
@@ -5,15 +5,10 @@ class database(BASE):
     __tablename__ = "database"
     website = Column(String, primary_key=True)
     link = Column(String)
-    website2 = Column(String, primary_key=True)
-    link2 = Column(String)
 
     def __init__(self, website, link):
         self.website = website
         self.link = link
-    def __init__(self, website2, link2):
-        self.website2 = website2
-        self.link2 = link2
 
 
 database.__table__.create(checkfirst=True)
@@ -38,23 +33,4 @@ def update_link(website, link):
             link
         )
     SESSION.add(adder)
-def get_link2(website2):
-    try:
-        return SESSION.query(database).get(website)
-    except:
-        return None
-    finally:
-        SESSION.close()
-
-
-def update_link2(website2, link2):
-    adder2 = SESSION.query(database2).get(website2)
-    if adder2:
-        adder2.link2 = link2
-    else:
-        adder2 = database(
-            website2,
-            link2
-        )
-    SESSION.add(adder2)
     SESSION.commit()
